c8e0e899c9544b9b0c7cdf5daadbe5d49da6b73b
AminTabite <Aminepicode13@gmail.com>
2025-08-08 16:51:29 +0200

    si é spaccata la navbar

--- src/components/Mynavbar.jsx
+++ src/components/Mynavbar.jsx
@@ -1,25 +1,47 @@
 import { Container, Navbar, Nav } from "react-bootstrap";
-import { GiHamburgerMenu } from "react-icons/gi"; // import icona
+import { GiHamburgerMenu } from "react-icons/gi";
 import logomusic from "../assets2/logos/download.svg";
+import { BsSearch, BsHouseDoor, BsMic } from "react-icons/bs";
+import { Form } from "react-bootstrap";
 
 const Mynavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
+      <Container className="d-flex flex-lg-column">
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <GiHamburgerMenu color="red" size={30} />
         </Navbar.Toggle>
 
-        <Navbar.Brand className="mx-auto">
-          <img src={logomusic} alt="Apple Music Logo" height="30" />
-        </Navbar.Brand>
+        <div>
+          <Navbar.Brand className="mx-auto">
+            <img src={logomusic} alt="Apple Music Logo" height="30" />
+          </Navbar.Brand>
+        </div>
 
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav d-flex flex-lg-column">
+          <Form.Control
+            type="text"
+            placeholder="Cerca"
+            className="m-3 bg-dark text-danger"
+          />
+
+          <div>
+            <BsSearch className="text-danger" />
+          </div>
+          <div>
+            <BsHouseDoor className="text-danger" />{" "}
+          </div>
+          <div>
+            <BsMic className="text-danger" />{" "}
+          </div>
+
           <Nav className="ms-auto"></Nav>
         </Navbar.Collapse>
-        <Nav.Link href="#" className="red">
-          Accedi
-        </Nav.Link>
+        <div className="text-primary d-lg-none">
+          <Nav.Link href="#" className="red">
+            Accedi
+          </Nav.Link>
+        </div>
       </Container>
     </Navbar>
   );
