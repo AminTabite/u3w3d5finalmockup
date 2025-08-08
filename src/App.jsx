e700a5b1be52ef6493491df5f2d93300f5e9194b
AminTabite <Aminepicode13@gmail.com>
2025-08-08 09:31:55 +0200

    beginning

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Container } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <>
+      <Container>
+        {" "}
+        {/* */}
+        <Row>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default App;
